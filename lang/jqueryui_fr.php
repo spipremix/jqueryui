f15d300f475264f0a0ff87f7111eaf55ac890c82
eric <eric@smellup.net>
2012-05-18 15:23:19 +0000

    Correction du slogan et ajout du fichier de langue a priori pour le data picker. Faut maintenant transférer les items de spip vers le plugin jquery UI
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/jquery_ui@61393 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/jqueryui_fr.php
@@ -0,0 +1,26 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'afficher_calendrier' => 'Afficher le calendrier',
+
+	// D
+	'date_mois_10_abbr' => 'oct.',
+	'date_mois_11_abbr' => 'nov.',
+	'date_mois_12_abbr' => 'déc.',
+	'date_mois_1_abbr' => 'janv.',
+	'date_mois_2_abbr' => 'févr.',
+	'date_mois_3_abbr' => 'mars',
+	'date_mois_4_abbr' => 'avr.',
+	'date_mois_5_abbr' => 'mai',
+	'date_mois_6_abbr' => 'juin',
+	'date_mois_7_abbr' => 'juil.',
+	'date_mois_8_abbr' => 'août',
+	'date_mois_9_abbr' => 'sept.'
+
+);
+
+?>
