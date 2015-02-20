adbea77a18670fb57e071f73e349b3eb6de16609
sylvain <sylvain@oliver-fr.org>
2015-02-20 04:33:15 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/paquet-jqueryui_oc_ni_mis.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-jqueryui?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// J
+	'jqueryui_description' => 'Aqueu plugin implementa en SPIP la biblioutèca coumplementari jQuery UI qu’autourisa la creacioun de coumpousant gràficou dinàmicou : ounglet, drag & drop, barra de prougressioun...
+Li founciounalità d’aquela biblioutèca si divison en 3 categourìa :
+-* {{Li interacioun}}. Permeton de dounà dei prouprietà pròpi interessanti en dei elemen HTML, couma si poudé estre achapat e desplaçat embé la rateta (drag & drop), d’estre redimensiounat couma un fenestroun, o ben d’estre seleciounat couma dei icona soubre lou burèu dóu vouòstre OS.
+-* {{Lu Widget}}. Aquelu soun dei ouget « clau en man » que proupauson dei founciounalità de aut nivèu couma lou DatePicker per isemple, qu’afichà un calendari permetent de seleciounà una data.
+-* {{Lu efet}}. Enriquisson lu efet predefinit proupausat per jQuery (fadeIn, slideUp...). Gauch a jQuery UI, es per isemple poussible d’animà la prouprietà CSS background-color embé .animate() o ben d’ajustà dei transicioun dóu temp dei addClass/removeClass...',
+	'jqueryui_slogan' => 'Animacioun, efet e widget jQuery'
+);
+
+?>
