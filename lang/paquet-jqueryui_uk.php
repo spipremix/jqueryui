ca8b19881462ca6c1a08813c306d90ba7f9ae246
olly.olga <olly.olga@gmail.com>
2015-05-06 03:33:21 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue uk

---
+++ lang/paquet-jqueryui_uk.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-jqueryui?lang_cible=uk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// J
+	'jqueryui_description' => 'Цей  плагін реалізує в SPIP додаткові бібліотеки JQuery UI, що дозволяє створювати динамічні графічні компоненти: вкладки, drag & drop, прогрес-бари...
+Особливості цієї бібліотеки поділяються на 3 категорії:
+-* {{Взаємодія (Les interactions)}}.  Використовуються, щоб надати дуже цікаві властивості HTML елементам, такі як можливісті їх спіймати і переміщати за допомогою миші (перетягнути і кинути), змінювати як вікно або вибирати як іконки на робочому столі OS.
+-* {{Віджети (Les Widgets)}}. Вони є об’єктами "під ключ", що пропонують можливості високого рівня, такі, як, наприклад, DatePicker, який показує календар для вибору дати.
+-* {{Ефекти (Les effects)}}. Вони збагачують типові ефекти, що пропонує jQuery (fadeIn, slideUp...). З JQuery UI, наприклад, можна анімувати CSS фон -властивість background-color з .animate() або додати переходи з addClass/removeClass...',
+	'jqueryui_slogan' => 'Анімація, ефекти і плагіни jQuery'
+);
+
+?>
