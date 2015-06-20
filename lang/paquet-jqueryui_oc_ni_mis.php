aaf98a8b5f6bd9034a0bd3d759d66e06ee760f0b
spip <spip@oliver-fr.org>
2015-06-20 03:33:30 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/jquery_ui@90379 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-jqueryui_oc_ni_mis.php
+++ lang/paquet-jqueryui_oc_ni_mis.php
@@ -10,9 +10,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// J
 	'jqueryui_description' => 'Aqueu plugin implementa en SPIP la biblioutèca coumplementari jQuery UI qu’autourisa la creacioun de coumpousant gràficou dinàmicou : ounglet, drag & drop, barra de prougressioun...
 Li founciounalità d’aquela biblioutèca si divison en 3 categourìa :
--* {{Li interacioun}}. Permeton de dounà dei prouprietà pròpi interessanti en dei elemen HTML, couma si poudé estre achapat e desplaçat embé la rateta (drag & drop), d’estre redimensiounat couma un fenestroun, o ben d’estre seleciounat couma dei icona soubre lou burèu dóu vouòstre OS.
--* {{Lu Widget}}. Aquelu soun dei ouget « clau en man » que proupauson dei founciounalità de aut nivèu couma lou DatePicker per isemple, qu’afichà un calendari permetent de seleciounà una data.
--* {{Lu efet}}. Enriquisson lu efet predefinit proupausat per jQuery (fadeIn, slideUp...). Gauch a jQuery UI, es per isemple poussible d’animà la prouprietà CSS background-color embé .animate() o ben d’ajustà dei transicioun dóu temp dei addClass/removeClass...',
+-* {{Li interacioun}}. Permeton de dounà de prouprietà pròpi interessanti en dei elemen HTML, couma si poudé estre achapat e desplaçat embé la rateta (drag & drop), d’estre redimensiounat couma un fenestroun, o ben d’estre seleciounat couma de icona soubre lou burèu dóu vouòstre OS.
+-* {{Lu Widget}}. Aquelu soun de ouget « clau en man » que proupauson dei founciounalità de aut nivèu couma lou DatePicker per isemple, qu’afichà un calendari permetent da seleciounà una data.
+-* {{Lu efet}}. Enriquisson lu efet predefinit proupausat per jQuery (fadeIn, slideUp...). Gauch a jQuery UI, es per isemple poussible d’animà la prouprietà CSS background-color embé .animate() o ben d’ajustà de transicioun dóu temp dei addClass/removeClass...',
 	'jqueryui_slogan' => 'Animacioun, efet e widget jQuery'
 );
 
