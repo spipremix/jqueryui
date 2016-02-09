86a0753fc5d7861c77109e483e94fcdb844dc048
rporto <rporto@calepino.com>
2016-02-09 04:33:38 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue pt_br

--- lang/paquet-jqueryui_pt_br.php
+++ lang/paquet-jqueryui_pt_br.php
@@ -10,11 +10,11 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// J
-	'jqueryui_description' => 'Este plugin implementa a biblioteca complementar JQuery UI no SPIP, permitindo a criação de componentes gráficos dinâmicos: abas, drag & drop, barras de progressão...
-As funcionalidades desta biblioteca estão organizadas em três categorias:
--* {{As interações}}. Podem fornecer propriedades muito interessantes a elementos HTML, como a possibilidade de agarrar e deslocar elementos com o mouse (drag & drop), redimensioná-los como se fossem janelas ou de os selecionar como ícones do seu sistema operacional.
--* {{Os Widgets}}. São objetos prontos, que oferecem funcionalidades sofisticadas como, por exemplo, o DatePicker, que exibe um calendário para seleção de uma data.
--* {{Os efeitos}}. Enriquecem os efeitos padrão oferecidos pelo JQuery (fadeIn, slideUp...). Graças ao JQuery UI é, por exemplo, possível animar a propriedade CSS background-color com .animate() ou ainda adicionar transições às ações addClass/removeClass...', # MODIF
+	'jqueryui_description' => 'Este plugin implementa a biblioteca complementar JQuery UI, permitindo a criação de componentes gráficos dinâmicos: abas, drag & drop, barras de progressão...
+As funcionalidades desta biblioteca se dividem em três categorias:
+-* {{As interações}}. Podem fornecer propriedades muito interessantes a elementos HTML, como a possibilidade de arrastar e soltar elementos com o mouse (drag & drop), redimensioná-los como se fossem janelas ou de os selecionar como ícones do seu sistema operacional.
+-* {{Os Widgets}}. São objetos prontos, que oferecem funcionalidades sofisticadas, como o DatePicker, que exibe um calendário para seleção de uma data.
+-* {{Os efeitos}}. Enriquecem os efeitos padrão oferecidos pelo JQuery (fadeIn, slideUp...). Graças ao JQuery UI é possível, por exemplo, animar a propriedade CSS background-color com .animate() ou ainda adicionar transições às ações addClass/removeClass...',
 	'jqueryui_slogan' => 'Animações, efeitos e widgets jQuery'
 );
 
