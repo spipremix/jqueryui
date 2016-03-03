605dfb64d337de04b93853a553f73516fdc3427b
spip <spip@allesfrans.com>
2016-03-03 04:34:49 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue nl

--- lang/paquet-jqueryui_nl.php
+++ lang/paquet-jqueryui_nl.php
@@ -10,12 +10,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// J
-	'jqueryui_description' => 'Deze plugin implementeert het extra bibliotheek jQuery UI in SPIP waardoor de creatie van dynamische grafische tabs, drag & drop, vooruitgang bars mogelijk wordt...
+	'jqueryui_description' => 'Deze plugin implementeert het extra jQuery UI bibliotheek in SPIP waardoor de creatie van dynamische grafische tabs, drag & drop, vooruitgangsbalken mogelijk wordt...
 Kenmerken van deze bibliotheek bestaan uit drie categorieën:
--* {{Interacties}}. Zij kunnen zeer interessante eigenschappen aan HTML-elementen geven, zoals de mogelijkheid om ze te kunnen grijpen en verplaatsen met de muis (drag & drop), aanpassen als een venster, of nog geselecteerd te worden als pictogrammen op het bureau van uw OS.
--* {{Widgets}}. Het zijn objecten "klaar voor gebruik" die een hoog niveau van functionaliteit aanbieden, zoals bijvoorbeeld  datepicker die een kalender aanmaakt om het selecteren van een datum te toelaten.
--* {{De effecten}}. Ze verrijken  de effecten aangeboden door jQuery (FadeIn, slideUp ...). Dankzij jQuery UI is het bijvoorbeeld mogelijk om de CSS background-color met .animate() te animeren of nog overgangen tijdens addClass / removeClass toe te voegen ...',
-	'jqueryui_slogan' => 'jQuery Animatie, effecten en widgets '
+-* {{Interacties}}. Ze kunnen zeer interessante eigenschappen aan HTML-elementen geven, zoals de mogelijkheid om ze te kunnen verplaatsen met de muis (drag & drop), aanpassen als een venster, of geselecteerd worden als pictogrammen op het bureaublad van je computer.
+-* {{Widgets}}. Dit zijn "kant-en-klare" objecten die een hoog niveau van functionaliteit bieden, zoals bijvoorbeeld datepicker die een kalender aanmaakt om het selecteren van een datum toe te laten.
+-* {{De effecten}}. Deze verrijken  de effecten aangeboden door jQuery (FadeIn, slideUp ...). Dankzij jQuery UI is het bijvoorbeeld mogelijk om de CSS background-color met .animate() te animeren of overgangen bij addClass / removeClass toe te voegen ...',
+	'jqueryui_slogan' => 'jQuery animatie, effecten en widgets '
 );
 
 ?>
