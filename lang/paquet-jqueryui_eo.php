00ad9ea2db0129eaa552d7ca598b0fe19957d854
gpl <gpl@long-player.net>
2018-03-07 04:36:37 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-jqueryui_eo.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-jqueryui?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// J
+	'jqueryui_description' => 'Tiu kromprogramo efektivigas en SPIP la aldonan bibliotekon „jQuery UI“, kiuj ebligas la kreon de grafikaj elementoj dinamikaj : langetoj, ŝovo-kaj-demeto, plenumskalo...
+La funkcioj de tiu biblioteko distribuiĝas triparte :
+-* {{Interagoj}}. Ili ebligas doni tre interesajn funkciojn al HTML-markoj, ekzemple, ŝovo kaj demeto per la muso, regrandigo kiel fenestro, aŭ elekto sammaniere kiel la labortablaj bildsimboloj de via operaciumo. 
+-* {{Kromprogrametoj}} aŭ {{fenestraĵoj}}. Ili estas tutpretaj objektoj, kiuj proponas elstarajn eblojn, kiel la „DatePicker“, ekz., kiuj montras kalendaron por elekti daton. 
+-* {{Efektoj}}. Ili pliriĉigas la defaŭltajn efektojn de jQuery (fadeIn, slideUp...). Danke al jQuery UI, eblas ekzemple animacii la CSS-atributon background-color per .animate(), aŭ aldoni transpasojn dum addClass/removeClass...',
+	'jqueryui_slogan' => 'Animacioj, efektoj kaj kromprogrametoj el jQuery'
+);
