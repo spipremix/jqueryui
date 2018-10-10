56021404891120cd591b610d174686751f498a47
origenius <origenius@mvb.biglobe.ne.jp>
2018-10-10 03:40:12 +0000

    [Salvatore] [source:_core_/plugins/jquery_ui/lang/ paquet-jqueryui] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/paquet-jqueryui_ja.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-jqueryui?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// J
+	'jqueryui_description' => 'このプラグインは、jQuery UIの補完ライブラリをSPIPに実装しています。それは、タブ、ドラッグアンドドロップ、プログレスバーなど、動的なグラフィックコンポーネントの作成を可能にします...
+このライブラリの機能は3つのカテゴリに分かれています：
+-  * {{インタラクション}}
+それらは、HTML要素に非常に興味深いプロパティを与えることを可能にします。例えば、HTML要素は、マウス（ドラッグアンドドロップ）でキャッチして移動すること、ウィンドウとしてサイズ変更すること、デスクトップ上のアイコンとして選択することができます。
+ -  * {{ウィジェット}}
+これらは、日付を選択するカレンダーを表示するDatePickerなどの高度な機能を提供する「ターンキー」オブジェクトです。
+ -  * {{効果}}
+それらは、jQuery（fadeIn、slideUp...）によって提供されるデフォルトのエフェクトを充実させます。jQuery UIのおかげで、.animate()を使ってCSSの背景色プロパティをアニメーション化することや、addClass / removeClass中にトランジションを追加することなどが可能です。',
+	'jqueryui_slogan' => 'インタラクション、効果、jQueryウィジェット'
+);
